c25933fe055946c982c054343b61cf896efa7f38
Jay <jasonnkemcha712@gmail.com>
2024-02-09 16:40:59 +0000

    Agriculture never fails

---
+++ herman/file.sh
@@ -0,0 +1,6 @@
+echo "Welcome to the Njuafac and son's Factory"
+echo "please enter user name for your account to be created"
+read username entered is $username
+sudo useradd $username
+echo "$username account created successfully"
+echo "It's nice doing business with you!!"
